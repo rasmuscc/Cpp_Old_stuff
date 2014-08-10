2dcbac15238f553b0edd461874b62b6223d7c764
Rasmus C. <sexygaltab@gmail.com>
2014-08-10 18:37:41 +0200

    Update primenumber.cpp

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,3 +1,8 @@
+/*
+Finder primtal og gemmer dem i en dynamisk allokeret array.
+Rasmus C. Christensen - 30-07-2014
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -19,7 +24,7 @@ bool isprime(unsigned int numb)
     return true;
 }
 
-int *growarray(int* p_array, int size)
+int *growarray(int* p_array, int size) // Husk * ved growarray.
 {
     int *p_temp_array = new int [size*2]; // lav en ny array der er dobbelt så stor.
 
@@ -27,29 +32,45 @@ int *growarray(int* p_array, int size)
     {
         p_temp_array[i]=p_array[i];
     }
-    delete[] p_array;
+    delete[] p_array; // pointer, derfor original værdi.
     return p_temp_array;
 }
 
 int main ()
 {
-    int size=10;
+
+    int size=100;
     int *p_array = new int [size];
     int ArrayIndex=0; // Holds the next array number.
+    int upto=1; // Max range.
+    int primecount=0; // Holds how many prime numbers are found. Used for printing.
 
-        for (unsigned int i=1;i<100000;i++) // Check primtal op til 1000 og fyld dem i memory fra heap.
+    while (upto>0)
     {
-        if (isprime(i)==true)
-        {
-            cout << i << endl;
-            p_array[ArrayIndex]=i;
-            ArrayIndex++;
+        cout << "Enter max range (0 to exit):";
+        cin >> upto;
 
-            if (ArrayIndex + 1==size) // Double array hvis den ikke er stor nok.
+        for (unsigned int i=1;i<upto;i++) // Check primtal op til 1000 og fyld dem i memory fra heap.
+        {
+            if (isprime(i)==true)
             {
-                p_array = growarray(p_array, size);
-                size = size*2; // Så vi bare ganger 100 med 2, men 200 med 2, osv....
+                primecount++;
+                cout << i << endl;
+                p_array[ArrayIndex]=i;
+                ArrayIndex++;
+
+                if (ArrayIndex + 1==size) // Double array hvis den ikke er stor nok.
+                {
+                    p_array = growarray(p_array, size);
+                    size = size*2; // Så vi ikke ganger 100 med 2 hvergang.
+                }
             }
         }
+        cout << "Printing array..." << endl;
+        for (int j=0;j<primecount;j++)
+            {
+                cout << p_array[j] << endl;
+            }
     }
 }
+
